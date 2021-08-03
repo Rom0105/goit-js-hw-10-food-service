20a7a46af8f609e6b4752dbeaf67676c624bec97
Rom0105 <savosko.photo@gmail.com>
2021-08-03 17:47:35 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import menuFoodTpl from './templates/food-menu.hbs';
+import menu from './js/menu.json';
 
+const ulListMenu = document.querySelector('.js-menu');
+const cardMenu = createMenuCardsMarkup(menu);
+ulListMenu.insertAdjacentHTML('beforeend', cardMenu);
+
+function createMenuCardsMarkup(menu) {
+  return menu.map(menuFoodTpl).join('');
+}
